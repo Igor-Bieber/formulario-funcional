c72ea23bccdb63b0a5aca380f577655cb455beb8
Igor Bieber <caveralol12345@Gmail.com>
2022-05-10 23:00:44 -0300

    desenvolvendo JS, confirmação de senha aplicado, validação de email aplicado e username n pode ficar vazio aplicado

--- scripts.js
+++ scripts.js
@@ -1,39 +1,59 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const senha = document.getElementById('senha')
-const senhaConfirm = document.getElementById('senha-confirm')
-
-form.addEventListener ("submit",(e) =>{
-    /*Serve para não recarregar a página ao clicar no botão de enviar*/ 
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("senha");
+const passwordConfirmation = document.getElementById("senhaConfirm");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     checkInputs();
+  });
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+    const senhaConfirmValue = senhaConfirm.value;
 
-});
-
-/*Função que irá checar se os inputs são validos ou inválidos e
-transforma-los de acordo com o status.*/ 
-function checkInputs(){
-    const usernameValor = username.value;
-    const emailValor = email.value;
-    const senhaValor = senha.value;
-    const senhaConfirmValor = senhaConfirm.value;
-
-    if (usernameValor == ""){
+    /*faz dar erro se não tiver nada escrito no campo de usuário*/ 
+    if (usernameValue === ""){
         setErrorFor(username,"O nome de usuário é obrigatório.");
+    } else{
+        setSuccessFor (username);
+    }
+    
+    /*Faz dar erro se as senhas não forem iguais*/ 
+    if (senhaConfirmValue !== senhaValue){
+        setErrorFor (senha,"As senhas não conferem");
+        setErrorFor (senhaConfirm,"As senhas não conferem");
+    }else{
+        setSuccessFor (senha);
+        setSuccessFor (senhaConfirm);
+    }
+
+    if (emailValue === ""){
+        setErrorFor(email,"O email é obrigatório.");
+    }else if(!checkEmail(emailValue)){
+        setErrorFor(email,"O email não é válido.");
+    }else{
+        setSuccessFor(email);
     }
 }
-
-function setErrorFor (input, message){
+function setErrorFor (input, message) {
+    /*input.parentElemente = puxa o elemento PAI do INPUT*/ 
     const formControl = input.parentElement;
     const small = formControl.querySelector("small");
-
-    /*Adicionando mensagem de erro*/ 
+    //exibe a mensagem de erro
     small.innerText = message;
-
-    /*Mudando o nome do CLASS da div adicionando a palavra "error"
-    no final para mostrar a mensagem de erro e a borda vermelha*/ 
+    /*muda o nome da classe adicionando o erro la no html*/
     formControl.className = "form-control error";
-
 }
+
+function setSuccessFor (input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
